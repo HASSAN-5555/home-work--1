4ed3c746f15a6052ebdbe72303aeee03340341a1
Sibtul Hassan <hassan0007267@gmail.com>
2024-08-06 20:55:33 +0500

    git

---
+++ class_1.py
@@ -0,0 +1,27 @@
+#question 1
+print("hello world")
+print("Hassan Ali")
+#question 2
+print("Hassan Ali")
+print(1,2,3)
+#question 3
+print("Hello\nWorld")
+print("Hello\tWorld")
+#question 4
+print("Apple", "Banana", "Mango",sep='')
+print(1,2,3,sep='')
+#question 5
+print("Hello" , end='')
+print(1, end='')
+print(2)
+#question 6
+print("Quaid e Azam said:" , "Expect the best, prepare for the worst")
+print("This is a backslash:\\")
+#question 7
+message=17
+print(f"i am {message} year old")
+age=10
+print(f"The number is {age}")
+#question 8
+for x in range(1,6):
+ print(x)
